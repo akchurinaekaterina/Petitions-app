b17e546594b25875e6d02460e8713cf512e80b73
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-25 21:33:02 +0300

    finished the challenge

--- Petitions app/ViewController.swift
+++ Petitions app/ViewController.swift
@@ -8,13 +8,17 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    
+    var urlString: String!
     var petitions = [Petition]()
+    var filteredPetitions = [Petition]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let urlString: String
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .bookmarks, target: self, action: #selector(showCredits))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(filterPetitions))
+        
+        
         
         if navigationController?.tabBarItem.tag == 0 {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
@@ -22,17 +26,9 @@ class ViewController: UITableViewController {
             urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
         }
         
+        loadData(from: urlString)
         
         
-        if let url = URL(string: urlString) {
-            if let data = try? Data(contentsOf: url) {
-                parse(json: data)
-            } else {
-                showError()
-            }
-        } else {
-            showError()
-        }
         // Do any additional setup after loading the view.
     }
     
@@ -67,6 +63,48 @@ class ViewController: UITableViewController {
         let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default))
         present(ac, animated: true)
+    }
+    @objc func showCredits(){
+        let creditsAlert = UIAlertController(title: "nil", message: "the data comes from the We The People API of the Whitehouse", preferredStyle: .alert)
+        creditsAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(creditsAlert, animated: true, completion: nil)
+    }
+    @objc func filterPetitions(){
+        let filterAC = UIAlertController(title: "Enter Search text", message: nil, preferredStyle: .alert)
+        filterAC.addTextField(configurationHandler: nil)
+        filterAC.addAction(UIAlertAction(title: "Search", style: .default, handler: { [weak filterAC, weak self] action in
+            if let text = filterAC?.textFields?[0].text {
+                self?.filter(text)
+            }
+        }))
+        filterAC.addAction(UIAlertAction(title: "Show all", style: .cancel, handler: { [weak self] action in
+            if let urlString = self?.urlString {
+            self?.loadData(from: urlString)
+            }
+        }))
+        present(filterAC, animated: true, completion: nil) 
+    }
+    func filter(_ text: String){
+        for petition in petitions {
+            if petition.title.contains(text) || petition.body.contains(text) {
+                filteredPetitions.append(petition)
+            }
+        }
+        petitions = filteredPetitions
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    func loadData(from urlString: String) {
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+            } else {
+                showError()
+            }
+        } else {
+            showError()
+        }
     }
 
 }
