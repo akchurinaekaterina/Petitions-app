6beba623876e2501472a9485350b57a16d4c9014
Ekaterina Akchurina <akchurina.ekaterina@gmail.com>
2020-09-25 13:10:04 +0300

    Initial Commit

---
+++ Petitions app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Petitions app
+//
+//  Created by Ekaterina Akchurina on 25.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
